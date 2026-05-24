a58cc72a6526d36e2ff70795c27991db96a0d04e
Ally-331a <4675309@qq.com>
2026-05-24 17:00:54 +0800

    Create AppTest.java

---
+++ src/main/java/com/mycompany/app/src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,12 @@
+package com.mycompany.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+    @Test
+    void testMessage() {
+        App app = new App();
+        assertEquals("Hello World!", app.getMessage());
+    }
+}
